fed9c062f6c30584af2a5eeccbef46c1562f31ce
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-13 16:52:02 -0400

    Adding new code

--- Touristy/Utilities/APIClient.swift
+++ Touristy/Utilities/APIClient.swift
@@ -7,3 +7,47 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
+
+protocol Routable {
+    var method: String { get }
+}
+
+enum MethodRouter: Routable {
+    case GET, POST
+    var method: String {
+        switch self {
+        case POST: return "POST"
+        case GET: return "GET"
+        }
+    }
+}
+
+protocol Request {
+    var method:String { get }
+    var baseURL: String { get }
+    var URLPath: String { get }
+    var parameters: Dictionary<String, String> { get }
+    var headers: Dictionary<String, String> { get }
+}
+
+struct GetRequest: Request {
+    var method: String { return MethodRouter.POST.method }
+    var baseURL: String
+    var URLPath: String
+    var parameters: Dictionary<String, String>
+    var headers: Dictionary<String, String>
+    
+    init(baseURL: String, URLPath:String, parameters: Dictionary<String, String>, headers:Dictionary<String, String>) {
+        self.baseURL = baseURL
+        self.URLPath = URLPath
+        self.parameters = parameters
+        self.headers = headers
+    }
+    
+    func sendGetRequest {
+    
+    }
+}
