8071abe0cc27604b8b496f608163695ca4b4234d
chriswebb09 <christopher.webb@flatironschool.com>
2016-08-17 18:44:37 -0400

    Added SplashScreen.swift and SplashScreenViewController.swift

---
+++ Touristy/Controllers/SplashSceenViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SplashSceenViewController.swift
+//  Touristy
+//
+//  Created by Christopher Webb-Orenstein on 8/17/16.
+//  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
